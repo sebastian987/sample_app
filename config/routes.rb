a053f3e6416042f9f913bf958a11682ef23bd207
sebastian <syang@rocketfuelinc.com>
2011-01-07 17:01:39 -0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ ActionController::Routing::Routes.draw do |map|
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
-  map.home '/', :controller => 'pages', :action => 'home'
-   map.root :controller => 'pages', :action => 'home'
-
+  map.home    '/',        :controller => 'pages', :action => 'home'
+  map.root                :controller => 'pages', :action => 'home'
+  map.signup '/signup',   :controller => 'users', :action => 'new'
 
 
 
